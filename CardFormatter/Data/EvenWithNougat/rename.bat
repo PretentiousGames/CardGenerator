07f17aea2fd58daff144c6eecc6fa0002336d168
Mike Brown <MBrown3@paylocity.com>
2020-05-05 05:01:09 -0600

    Even with nougat

---
+++ CardFormatter/Data/EvenWithNougat/rename.bat
@@ -0,0 +1,26 @@
+move Blue.png Blue.tmp.png
+move Cyan.png Cyan.tmp.png
+move Green.png Green.tmp.png
+move Magenta.png Magenta.tmp.png
+move Orange.png Orange.tmp.png
+move Purple.png Purple.tmp.png
+move Red.png Red.tmp.png
+move Yellow.png Yellow.tmp.png
+
+move Blue.2.png Blue.png
+move Cyan.2.png Cyan.png
+move Green.2.png Green.png
+move Magenta.2.png Magenta.png
+move Orange.2.png Orange.png
+move Purple.2.png Purple.png
+move Red.2.png Red.png
+move Yellow.2.png Yellow.png
+
+move Blue.tmp.png Blue.2.png
+move Cyan.tmp.png Cyan.2.png
+move Green.tmp.png Green.2.png
+move Magenta.tmp.png Magenta.2.png
+move Orange.tmp.png Orange.2.png
+move Purple.tmp.png Purple.2.png
+move Red.tmp.png Red.2.png
+move Yellow.tmp.png Yellow.2.png
